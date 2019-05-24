8f27dce0259444b9c87c6ae751c725e84f864b6e
Katarina Cavar <kcavar96@gmail.com>
2019-05-24 15:59:45 +0200

    add book to database, print text to /booklist

---
+++ Crud_Razor_2_1/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Crud_Razor_2_1.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+      public string someData { get; set; }
+      public void OnGet()
+      {
+      someData = "This is first Property!";
+      }
+    }
+}
